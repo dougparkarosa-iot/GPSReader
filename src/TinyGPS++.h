08a70f40d5b2292a0bda7de3575376661e1e422e
Doug Park <dougparkarosa@gmail.com>
2022-06-21 23:33:39 -0700

    Commented spurious include.

--- src/TinyGPS++.h
+++ src/TinyGPS++.h
@@ -27,7 +27,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
-#include "WProgram.h"
+//#include "WProgram.h"
 #endif
 #include <limits.h>
 
